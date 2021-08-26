4d2a399c3fa54a0034fce9dee539b8e0b39d4f82
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-08-26 18:37:02 -0400

    Add project files.

---
+++ ASPDemo2/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPDemo2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Mary";
+            ViewBag.FV = 99999.99;
+            return View();
+        }
+    }
+}
